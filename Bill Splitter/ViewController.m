689cebeba356d4940b42488559642ccd7f64d4ec
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-12 01:08:12 -0700

    assignment completed; stretch goals TBD
    Stretch Goals
    Include tip in the calculation
    Remove the need for a CALCULATE button. Instead have the app automatically calculate when the total amount or number of people inputs are modified. (HINT: UITextFieldDelegate!)

--- Bill Splitter/ViewController.m
+++ Bill Splitter/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *totalBillAmount;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleForBill;
+@property (weak, nonatomic) IBOutlet UILabel *portionForEachPerson;
+@property (weak, nonatomic) IBOutlet UIButton *calculateBillPortionsPressed;
+
+- (IBAction)calculateSplitAmount:(id)sender;
 
 @end
 
@@ -16,14 +22,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    self.portionForEachPerson.text = @"9.99";
+//     Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)sliderValueChanged:(id)sender {
+    //below code ensures slider only uses full int's
+    // http://iphonedevsdk.com/forum/iphone-sdk-development/81746-how-to-show-integer-values-for-ui-slider.html
+    UISlider *slider = (UISlider *)sender;
+    NSString *newText = [[NSString alloc] initWithFormat:@"%d", (int)slider.value];
+    self.portionForEachPerson.text = newText;
 }
 
+
+
+- (IBAction)calculateSplitAmount:(id)sender{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc]init];
+    numberFormatter.locale = [NSLocale currentLocale];// this ensures the right separator behavior
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+    [numberFormatter setMaximumFractionDigits:2];
+    numberFormatter.usesGroupingSeparator = YES;
+    
+
+    NSDecimalNumber *numberOfPeople = [[NSDecimalNumber alloc] initWithFloat:self.numberOfPeopleForBill.value];
+    NSDecimalNumber *totalBill = [[NSDecimalNumber alloc] initWithString:self.totalBillAmount.text];
+
+    
+    //totalBill = [self.totalBillAmount.text intValue];
+    
+    totalBill = [totalBill decimalNumberByDividingBy:numberOfPeople];
+//    self.totalBillAmount.text = [NSNumberFormatter totalBill];
+//    self.totalBillAmount.text = [NSString stringWithFormat:@"%.02@", totalBill ];
+    
+    self.totalBillAmount.text = [NSString stringWithFormat:@"%@", [numberFormatter stringForObjectValue:totalBill]];
+
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
